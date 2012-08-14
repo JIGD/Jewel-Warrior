005cdf81abc28936950d6e3116a691d1252284c9
José Ignacio <igonzalez@nearsoft.com>
2012-08-14 00:16:19 -0700

    agregados perks para dispositivos de apple

--- scripts/loader.js
+++ scripts/loader.js
@@ -7,6 +7,9 @@ var jewel = {
 //wait until the doc is loaded
 window.addEventListener("load", function(){
 	//start dynamic loading
+    Modernizr.addTest("standalone", function(){
+        return (window.navigator.standalone != false);
+    });
 	Modernizr.load([
 		{
 			//these files are always loaded
@@ -14,14 +17,25 @@ window.addEventListener("load", function(){
 			"scripts/sizzle.js",
 			"scripts/dom.js",
 			"scripts/game.js",
-            "scripts/screen.main-menu.js",
-            "scripts/screen.splash.js"
-			],
-			//called when all files finished loading and executing
+			]
+        }, {
+            test: Modernizr.standalone,
+            yep : "scripts/screen.splash.js",
+            nope: "scripts/screen.install.js",
+           //called when all files finished loading and executing
 			complete: function(){
+                if(Modernizr.standalone){
 				jewel.game.showScreen("splash-screen");
-				alert("All files loaded!");
+                }
+                else{
+                jewel.game.showScreen("install-screen");
+                }
 			}
 			}
 	]);
+    if(Modernizr.standalone){
+        Modernizr.load([
+            "scripts/screen.main-menu.js"
+        ]);
+    }
 }, false);
